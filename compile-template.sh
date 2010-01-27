badd473b57c6c5e0fab9c61b3c0615a039ad7940
Wu Yongzheng <wuyongzheng@gmail.com>
2010-01-27 14:56:31 +0800

    fix bug

--- compile-template.sh
+++ compile-template.sh
@@ -6,8 +6,8 @@ if [ ! -f config.rc ] ; then
 fi
 source config.rc
 
-grep '#DEFVAR' config.rc | sed -e 's/.*DEFVAR //g' -e 's/:.//g' | tr ' ' '\n' | gawk 'BEGIN{x=2} {print "s/%"$1"%/"x"/g"; x++;}' > compile-template.sed
-echo "s/%interval%/$INTERVAL/g"
+grep '#DEFVAR' config.rc | sed -e 's/.*DEFVAR //g' -e 's/:.//g' | tr ' ' '\n' | gawk 'BEGIN{x=2} {print "s/%"$1"%/"x"/g"; x++;}' >compile-template.sed
+echo "s/%interval%/$INTERVAL/g" >>compile-template.sed
 
 sed -f compile-template.sed <gnuplot.template >gnuplot.plot
 
